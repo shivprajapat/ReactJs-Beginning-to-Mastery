ecbae9664f4dfcf809d887752af0bb747831e11f
shivprajapat <shivkumarprajapat232@gmail.com>
2022-10-10 14:35:26 +0530

    state use class & function component

--- src/class/Cls.js
+++ src/class/Cls.js
@@ -1,11 +1,20 @@
 import React, { Component, Fragment } from 'react'
 
 export default class Cls extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: "Hooks use Class Components" }
+  }
+  clickMe() {
+    this.setState({ message: "Click messages" })
+  }
   render() {
     const { name } = this.props
     return (
       <Fragment>
         <h2>{name}</h2>
+        <p>{this.state.message}</p>
+        <button onClick={() => this.clickMe()}>Click Me</button>
       </Fragment>
     )
   }
--- src/function/Fun.js
+++ src/function/Fun.js
@@ -1,9 +1,15 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
-const Fun = ({name}) => {
+const Fun = ({ name }) => {
+    const [message, setMessage] = useState("Hooks use Function Components")
+    const clickMe=()=>{
+        setMessage("Click messages")
+    }
     return (
         <Fragment>
             <h2>{name}</h2>
+            <p>{message}</p>
+            <button onClick={clickMe}>Click Me</button>
         </Fragment>
     )
 }
