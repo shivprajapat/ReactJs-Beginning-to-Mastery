3d7a8de1bf364e9bce1803e074236a372c8b761a
shivprajapat <shivkumarprajapat232@gmail.com>
2022-10-10 16:39:39 +0530

    Conditional Rendering use class component

--- src/class/Cls.js
+++ src/class/Cls.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
-import Parent from './Parent';
 
 export default class Cls extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoggedIn: false,
+    }
+
+  }
+  isLoggedIn() {
+    if (this.state.isLoggedIn) {
+      return <p>Hello Shiv </p>
+    } else {
+      return <p>Hello Guest </p>
+
+    }
+  }
+  isLoggedInTernary() {
+    return (
+      <>
+        <b>Ternary Operator</b><p>
+          Hello {this.state.isLoggedIn ? "Shiv" : "Gusest"}</p>
+      </>
+    )
+  }
   render() {
+
     return (
       <div>
         <h2>Class Components</h2>
-        <Parent />
+        {this.isLoggedIn()}
+        {this.isLoggedInTernary()}
+        {this.state.isLoggedIn && <p>Welcome Shiv</p>}
       </div>
     )
   }
--- src/function/Fun.js
+++ src/function/Fun.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Parent from './Parent';
 
 const Fun = () => {
   return (
     <div>
       <h2>Function Components</h2>
-      <Parent />
     </div>
   )
 }
