57216182a27f25d95e19f48650661d2073533722
shivprajapat <shivkumarprajapat232@gmail.com>
2022-10-10 16:02:29 +0530

    bind use class component

--- src/class/Cls.js
+++ src/class/Cls.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
 
 export default class Cls extends Component {
-  clickMe(){
-    alert('event clicked');
+  constructor() {
+    super()
+    this.state = {
+      name: "Hello"
+    }
+  }
+
+  clickMe() {
+    this.setState({ name: "World" })
+    console.log(this);
   }
   render() {
     return (
       <div>
         <h2>Class Components</h2>
-        <button onClick={this.clickMe}>click me</button>
+        <p>{this.state.name}</p>
+        <button onClick={this.clickMe.bind(this)}>click me</button>
       </div>
     )
   }
